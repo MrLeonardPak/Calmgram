7851ae274b747e9f8672e20a5208bdebf18c676b
Leonard Pak <leopak2000@gmail.com>
2022-04-15 13:09:24 +0300

    Add entity.h

---
+++ api_server/entity/entity.h
@@ -0,0 +1,35 @@
+#ifndef CALMGRAM_API_SERVER_ENTITY_H
+#define CALMGRAM_API_SERVER_ENTITY_H
+
+#include <string>
+#include <vector>
+
+namespace calmgram::api_server::entity {
+
+enum TypeOfContent { TEXT, JPEG };
+
+struct Content {
+  std::string data;
+  TypeOfContent type;
+};
+
+struct Message {
+  int owner_id;
+  time_t created;
+  Content content;
+  bool is_marked;
+};
+
+struct Chat {
+  int id;
+  std::vector<Message> msgs;
+};
+
+struct User {
+  int id;
+  std::vector<Chat> chats;
+};
+
+}  // namespace calmgram::api_server::entity
+
+#endif  // CALMGRAM_API_SERVER_ENTITY_H
