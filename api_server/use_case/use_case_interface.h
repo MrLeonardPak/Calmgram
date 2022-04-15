27518642c0a9ca9741a7db034a01237dea44f6df
Leonard Pak <leopak2000@gmail.com>
2022-04-15 13:30:17 +0300

    Add interfaces for use cases

---
+++ api_server/use_case/use_case_interface.h
@@ -0,0 +1,48 @@
+#ifndef CALMGRAM_API_SERVER_USE_CASE_INTERFACE_H
+#define CALMGRAM_API_SERVER_USE_CASE_INTERFACE_H
+
+#include "entity.h"
+
+namespace calmgram::api_server::use_case {
+
+namespace entity = ::calmgram::api_server::entity;
+
+class IGetUser {
+ public:
+  virtual ~IGetUser(){};
+  virtual entity::User GetUser(int id);
+};
+
+class ICreateUser {
+ public:
+  virtual ~ICreateUser() {}
+  virtual int CreateUser();
+};
+
+class ISetChat {
+ public:
+  virtual ~ISetChat() {}
+  virtual void SetChat(std::vector<int> users, int chat_id);
+};
+
+class ICreateChat {
+ public:
+  virtual ~ICreateChat() {}
+  virtual entity::Chat CreateChat(int id);
+};
+
+class IGetMsgs {
+ public:
+  virtual ~IGetMsgs() {}
+  virtual std::vector<entity::Message> GetMsgs(int chat_id, time_t from_time);
+};
+
+class ISendMsg {
+ public:
+  virtual ~ISendMsg() {}
+  virtual entity::Chat SendMsg(entity::Message const& msg, int chat_id);
+};
+
+}  // namespace calmgram::api_server::use_case
+
+#endif  // CALMGRAM_API_SERVER_USE_CASE_INTERFACE_H
