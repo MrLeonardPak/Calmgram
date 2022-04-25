83a0ea1914b5be2d6e8d2bf61aecbef4c4db1f97
Leonard Pak <leopak2000@gmail.com>
2022-04-25 20:35:14 +0300

    Remove constructors from entities

--- api_server/entities/inc/entities.h
+++ api_server/entities/inc/entities.h
@@ -33,17 +33,11 @@ struct Message {
 struct Chat {
   int id;
   std::vector<Message> msgs;
-
-  Chat(int id, std::vector<Message> msgs) : id(id), msgs(msgs) {}
-  Chat(int id) : id(id), msgs(std::vector<Message>()) {}
 };
 
 struct User {
   int id;
   std::vector<Chat> chats;
-
-  User(int id, std::vector<Chat> chats) : id(id), chats(chats) {}
-  User(int id) : id(id), chats(std::vector<Chat>()) {}
 };
 
 }  // namespace calmgram::api_server::entities
