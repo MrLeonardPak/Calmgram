12f3191bfc3cb47523f8b1021569e23614eff54a
Leonard Pak <leopak2000@gmail.com>
2022-04-27 14:51:15 +0300

    Update AddChatUC

--- api_server/use_case/inc/add_chat_uc.h
+++ api_server/use_case/inc/add_chat_uc.h
@@ -10,17 +10,17 @@ namespace calmgram::api_server::use_case {
 class AddChatUC {
  private:
   std::vector<int> users_;
-  IGetUser& getter_user_;
+  ICheckUser& checker_user_;
   ICreateChat& creater_chat_;
   ISetChat& setter_chat_;
 
  public:
   AddChatUC(std::vector<int> users,
-            IGetUser& getter_user,
+            ICheckUser& checker_user,
             ICreateChat& creater_chat,
             ISetChat& setter_chat)
       : users_(users),
-        getter_user_(getter_user),
+        checker_user_(checker_user),
         creater_chat_(creater_chat),
         setter_chat_(setter_chat) {}
   ~AddChatUC() {}
