5d088b63b3cdb1716d7f1bcdc6ebfdc4a48a9a85
Leonard Pak <38388336+MrLeonardPak@users.noreply.github.com>
2022-05-11 23:39:21 +0300

    ml done (#30)
    Co-authored-by: howardano <58511197+howardano@users.noreply.github.com>

---
+++ ML/NN/src/classifier.cc
@@ -0,0 +1,17 @@
+#include "classifier.h"
+
+namespace calmgram::ml::nn {
+    void Classifier::Fit(/*calmgram::ml::data::Vectorizer x_train, std::vector<int> y_train*/) {
+
+    }
+
+    bool Classifier::Predict(/*calmgram::ml::data::Vectorizer x_test*/) {
+        return true;
+    }
+
+    std::vector<float> Classifier::PredictProba(/*calmgram::ml::data::Vectorizer x_test*/) {
+        std::vector<float> ret;
+        return ret;
+    }
+}
+
